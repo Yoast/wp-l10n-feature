9e3d4c0351c70bb0eaee03838410fd523bdfe530
Ari Stathopoulos <aristath@gmail.com>
2023-04-03 14:48:52 +0300

    convert to class

--- wpl10n-feature.php
+++ wpl10n-feature.php
@@ -4,133 +4,157 @@
  */
 
 /**
- * Get translations from a JSON file.
- * This function does the following:
- *     - If the file doesn't exist, create it from the .mo file.
- *     - If the .mo file is newer than the .json file, regenerate the .json file.
- *     - If the .json file exists, return the translations.
- *     - If the .json file cannot be created, return false.
- *
- * @param string $domain Text domain. Unique identifier for retrieving translated strings.
- * @param string $mofile Path to the MO file.
- * @param string $locale The locale of the file.
- *
- * @return array|bool Array of translations or false if the JSON file cannot be created.
+ * The main plugin class.
  */
-function wpl10n_get_translations( $domain, $mofile, $locale ) {
+class WPL10n_Feature {
 
-	// TODO: If the file exists, check the date. If the date is older than the .mo file, regenerate the .json file.
-	$json_path = str_replace( '.mo', '.json', $mofile );
+	/**
+	 * Class constructor.
+	 */
+	public function __construct() {
+		add_filter( 'override_load_textdomain', array( $this, 'override_load_textdomain' ), 10, 4);
+	}
 
-	// Just init an empty array to avoid errors down the line.
-	$translations = array();
-	if ( ! file_exists( $json_path ) ) {
-		global $wp_filesystem;
-		if ( ! $wp_filesystem ) {
-			require_once ABSPATH . '/wp-admin/includes/file.php';
-			WP_Filesystem();
-		}
+	/**
+	 * Get translations from a JSON file.
+	 * This function does the following:
+	 *     - If the file doesn't exist, create it from the .mo file.
+	 *     - If the .mo file is newer than the .json file, regenerate the .json file.
+	 *     - If the .json file exists, return the translations.
+	 *     - If the .json file cannot be created, return false.
+	 *
+	 * @param string $domain Text domain. Unique identifier for retrieving translated strings.
+	 * @param string $mofile Path to the MO file.
+	 * @param string $locale The locale of the file.
+	 *
+	 * @return array|bool Array of translations or false if the JSON file cannot be created.
+	 */
+	function get_translations_from_file( $domain, $mofile, $locale ) {
 
-		$mo = new \Mo();
-		$mo->import_from_file( $mofile );
+		// TODO: If the file exists, check the date. If the date is older than the .mo file, regenerate the .json file.
+		$json_path = str_replace( '.mo', '.json', $mofile );
 
-		// Get the Plural Forms header from the MO file.
-		$plural = 'nplurals=2; plural=n != 1;';
-		if ( isset( $mo->headers['Plural-Forms'] ) ) {
-			$plural = $mo->headers['Plural-Forms'];
-		}
+		// Just init an empty array to avoid errors down the line.
+		$translations = array();
+		if ( ! file_exists( $json_path ) ) {
+			global $wp_filesystem;
+			if ( ! $wp_filesystem ) {
+				require_once ABSPATH . '/wp-admin/includes/file.php';
+				WP_Filesystem();
+			}
 
-		// Build the JSON file structure.
-		$json_content = array(
-			'translation-revision-date' => filemtime( $mofile ),
-			'generator'                 => 'WordPress',
-			'domain'                    => 'messages',
-			'locale_data'               => array(
-				'messages'              => array(
-					'' => array(
-						'domain'       => 'messages',
-						'plural-forms' => $plural,
-						'lang'         => $locale,
+			$mo = new \Mo();
+			$mo->import_from_file( $mofile );
+
+			// Get the Plural Forms header from the MO file.
+			$plural = 'nplurals=2; plural=n != 1;';
+			if ( isset( $mo->headers['Plural-Forms'] ) ) {
+				$plural = $mo->headers['Plural-Forms'];
+			}
+
+			// Build the JSON file structure.
+			$json_content = array(
+				'translation-revision-date' => filemtime( $mofile ),
+				'generator'                 => 'WordPress',
+				'domain'                    => 'messages',
+				'locale_data'               => array(
+					'messages'              => array(
+						'' => array(
+							'domain'       => 'messages',
+							'plural-forms' => $plural,
+							'lang'         => $locale,
+						),
 					),
-				),
-			)
-		);
+				)
+			);
 
-		// Add translations to the JSON file.
-		foreach ( $mo->entries as $key => $entry ) {
-			$json_content['locale_data']['messages'][ $key ] = $entry->translations;
+			// Add translations to the JSON file.
+			foreach ( $mo->entries as $key => $entry ) {
+				$json_content['locale_data']['messages'][ $key ] = $entry->translations;
+			}
+
+			// Try to write the JSON file. If it fails, return false to fallback to the .mo file.
+			if ( ! $wp_filesystem->put_contents( $json_path, json_encode( $json_content, JSON_PRETTY_PRINT ) ) ) {
+				return false;
+			}
+
+			return $json_content['locale_data']['messages'][ $key ];
 		}
 
-		// Try to write the JSON file. If it fails, return false to fallback to the .mo file.
-		if ( ! $wp_filesystem->put_contents( $json_path, json_encode( $json_content, JSON_PRETTY_PRINT ) ) ) {
+		// Read the JSON file and save translations in the $translations variable.
+		$json_content = json_decode( file_get_contents( $json_path ), true );
+
+		// If the .mo file is newer than the .json file, delete the .json file
+		// so it can be re-generated on the next pageload.
+		if ( filemtime( $mofile ) > (int) $json_content['translation-revision-date'] ) {
+			$wp_filesystem->delete( $json_path );
+			return false;
+		}
+		return $json_content['locale_data']['messages'];
+	}
+
+	/**
+	 * Hooks in the `override_load_textdomain` filter to load translations from a JSON file.
+	 *
+	 * @param bool   $override Whether to override the textdomain load. Default false.
+	 * @param string $domain   Text domain. Unique identifier for retrieving translated strings.
+	 * @param string $mofile   Path to the MO file.
+	 * @param string $locale   The locale of the file.
+	 *
+	 * @return bool True if the translations were loaded from the JSON file, false otherwise.
+	 */
+	function override_load_textdomain( $override, $domain, $mofile, $locale ) {
+		/**
+		 * Filters MO file path for loading translations for a specific text domain.
+		 *
+		 * @since 2.9.0
+		 *
+		 * @param string $mofile Path to the MO file.
+		 * @param string $domain Text domain. Unique identifier for retrieving translated strings.
+		 */
+		$mofile = apply_filters( 'load_textdomain_mofile', $mofile, $domain );
+
+		/**
+		 * Fires before the MO translation file is loaded.
+		 *
+		 * @since 2.9.0
+		 *
+		 * @param string $domain Text domain. Unique identifier for retrieving translated strings.
+		 * @param string $mofile Path to the .mo file.
+		 */
+		do_action( 'load_textdomain', $domain, $mofile );
+
+		$translations = $this->get_translations_from_file( $domain, $mofile, $locale );
+
+		if ( ! $translations ) {
 			return false;
 		}
 
-		return $json_content['locale_data']['messages'][ $key ];
-	}
+		// Replace the translations with the ones from the JSON file for simple strings.
+		add_filter( "gettext_{$domain}", function( $translation, $text, $domain ) use ( $translations ) {
+			if ( ! empty( $translations[ $text ] ) ) {
+				return $translations[ $text ][0];
+			}
+		}, 10, 3 );
 
-	// Read the JSON file and save translations in the $translations variable.
-	$json_content = json_decode( file_get_contents( $json_path ), true );
+		// Replace the translations with the ones from the JSON file for strings with context.
+		add_filter( "gettext_with_context_{$domain}", function( $translation, $text, $context, $domain ) use ( $translations ) {
+			if ( ! empty( $translations[ "$context$text" ] ) ) {
+				return $translations[ $text ][0];
+			}
+		}, 10, 4 );
 
-	// If the .mo file is newer than the .json file, delete the .json file
-	// so it can be re-generated on the next pageload.
-	if ( filemtime( $mofile ) > (int) $json_content['translation-revision-date'] ) {
-		$wp_filesystem->delete( $json_path );
-		return false;
+
+		// Replace the translations with the ones from the JSON file for strings with plurals.
+		// TODO: Handle using the $plural expression.
+		add_filter( "ngettext_{$domain}", function( $translation, $single, $plural, $number, $domain ) use ( $translations ) {
+			if ( ! empty( $translations[ $single ] ) ) {
+				return $translations[ $single ][ $number == 1 ? 0 : 1 ];
+			}
+		}, 10, 5 );
+
+		return true;
 	}
-	return $json_content['locale_data']['messages'];
 }
 
-add_filter( 'override_load_textdomain', function( $override, $domain, $mofile, $locale ) {
-	/**
-	 * Filters MO file path for loading translations for a specific text domain.
-	 *
-	 * @since 2.9.0
-	 *
-	 * @param string $mofile Path to the MO file.
-	 * @param string $domain Text domain. Unique identifier for retrieving translated strings.
-	 */
-	$mofile = apply_filters( 'load_textdomain_mofile', $mofile, $domain );
-
-	/**
-	 * Fires before the MO translation file is loaded.
-	 *
-	 * @since 2.9.0
-	 *
-	 * @param string $domain Text domain. Unique identifier for retrieving translated strings.
-	 * @param string $mofile Path to the .mo file.
-	 */
-	do_action( 'load_textdomain', $domain, $mofile );
-
-	$translations = wpl10n_get_translations( $domain, $mofile, $locale );
-
-	if ( ! $translations ) {
-		return false;
-	}
-
-	// Replace the translations with the ones from the JSON file for simple strings.
-	add_filter( "gettext_{$domain}", function( $translation, $text, $domain ) use ( $translations ) {
-		if ( ! empty( $translations[ $text ] ) ) {
-			return $translations[ $text ][0];
-		}
-	}, 10, 3 );
-
-	// Replace the translations with the ones from the JSON file for strings with context.
-	add_filter( "gettext_with_context_{$domain}", function( $translation, $text, $context, $domain ) use ( $translations ) {
-		if ( ! empty( $translations[ "$context$text" ] ) ) {
-			return $translations[ $text ][0];
-		}
-	}, 10, 4 );
-
-
-	// Replace the translations with the ones from the JSON file for strings with plurals.
-	// TODO: Handle using the $plural expression.
-	add_filter( "ngettext_{$domain}", function( $translation, $single, $plural, $number, $domain ) use ( $translations ) {
-		if ( ! empty( $translations[ $single ] ) ) {
-			return $translations[ $single ][ $number == 1 ? 0 : 1 ];
-		}
-	}, 10, 5 );
-
-	return true;
-}, 10, 4);
-
+new WPL10n_Feature();
